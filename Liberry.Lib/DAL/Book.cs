249562821e016c049769ad32bf2cc471c49c7750
Matthew Bryant <matt23488@gmail.com>
2019-02-04 12:49:59 -0500

    Some more stuff.

---
+++ Liberry.Lib/DAL/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liberry.Lib.DAL
+{
+    class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string AuthorLast { get; set; }
+        public string AuthorFirst { get; set; }
+        public int SectionId { get; set; }
+
+        public virtual Section Section { get; set; }
+    }
+}
---
+++ Liberry.Web/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Liberry.Web.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Liberry.Web.Controllers
+{
+    [ValidateToken]
+    public class BooksController : ControllerBase
+    {
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
